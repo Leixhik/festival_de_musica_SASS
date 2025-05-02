f7e4c6ff2807c36d98115a2fdfb9ba78cfd76ff7
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-05-02 16:45:53 -0600

    Segun yo entendi esto: Importo lo que es gulp con src y dest para ubicar el scss, importo la implementacion de dartSass para compilar archivos SCSS, importo gulpSass para importal el paquete de gulp-sass que en si es Gulp, Finalmente creo una funcion donde utilizo src para encontrar el .scss que quiero compilar y por medio de un .pipe(sass()) y .pipe(dest()) le doy la ruta de donde quiero el compilador. Finalemente ejecuto npm run css para correr el script y hacer todo el compilado desde gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-export function hola(done) {
-    console.log("Hola desde Gulpfile.js");
+import {src, dest} from 'gulp'; // src y dest son funciones de gulp que permiten definir tareas para procesar archivos. src se utiliza para especificar la ubicación de los archivos de origen, mientras que dest se utiliza para definir la ubicación de salida donde se guardarán los archivos procesados.
 
-    done(); // Llama a la función de finalización para indicar que la tarea ha terminado
+import * as dartSass from 'sass'; // Dart Sass es una implementación de Sass escrita en Dart. Es la versión recomendada de Sass y es compatible con todas las características del lenguaje Sass. Dart Sass se utiliza para compilar archivos SCSS o Sass a CSS.
+
+import gulpSass from "gulp-sass";
+/* utiliza la sintaxis de módulos de JavaScript (ES Modules) para importar el paquete gulp-sass. Este paquete es un plugin para Gulp (un sistema de automatización de tareas) que permite compilar archivos SCSS o Sass a CSS. Sass es un preprocesador de CSS que añade características como variables, anidación y mixins, lo que facilita la escritura y mantenimiento de estilos. 
+*/
+
+const sass = gulpSass(dartSass);
+
+export function css(done){
+    src('src/scss/app.scss') // src('src/scss/app.scss') es la ruta de entrada del archivo SCSS que se va a compilar. Este archivo puede contener estilos y reglas CSS escritas en Sass.
+
+        .pipe(sass())
+        .pipe(dest('build/css'))
+
+    done();
 }
