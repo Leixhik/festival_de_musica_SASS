ff4b2585f00d63f34edbcea0b2a5ae507cf5078d
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-05-03 15:15:20 -0600

    Se ha creado el script para el watch de gulp, además se importaron fuentes en index, y en app.scss, el uso del framework funciona correctamente

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import {src, dest} from 'gulp'; // src y dest son funciones de gulp que permiten definir tareas para procesar archivos. src se utiliza para especificar la ubicación de los archivos de origen, mientras que dest se utiliza para definir la ubicación de salida donde se guardarán los archivos procesados.
+import {src, dest, watch} from 'gulp'; // src y dest son funciones de gulp que permiten definir tareas para procesar archivos. src se utiliza para especificar la ubicación de los archivos de origen, mientras que dest se utiliza para definir la ubicación de salida donde se guardarán los archivos procesados.
 
 import * as dartSass from 'sass'; // Dart Sass es una implementación de Sass escrita en Dart. Es la versión recomendada de Sass y es compatible con todas las características del lenguaje Sass. Dart Sass se utiliza para compilar archivos SCSS o Sass a CSS.
 
@@ -15,4 +15,8 @@ export function css(done){
         .pipe(dest('build/css'))
 
     done();
+}
+
+export function dev(){
+    watch('src/scss/app.scss', css);/* La línea observa cambios en el archivo app.scss y ejecuta la tarea css cuando detecta modificaciones */
 }
