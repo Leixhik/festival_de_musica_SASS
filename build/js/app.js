1e271ca47bf6aef8dffc98291286d01a9cc362c5
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-05-21 17:51:56 -0600

    Se agregó el fade in y fade out para las imagenes, además del botón de cerrar modal

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     crearGaleria()
 })
 
@@ -6,34 +6,55 @@ function crearGaleria() {
     const cantidadImagenes = 16
     const galeria = document.querySelector('.galeria-imagenes')
 
-    for( let i = 1; i <= cantidadImagenes ; i++){
+    for (let i = 1; i <= cantidadImagenes; i++) {
         const imagen = document.createElement('IMG')
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagen Galería'
 
         //Event Handler
-        imagen.onclick = function(){
+        imagen.onclick = function () {
             mostrarImagen(i)
         }
-        
+
         galeria.appendChild(imagen)
     }
 }
 
-function mostrarImagen(i){
-    
+function mostrarImagen(i) {
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen ${id} en tamaño completo';
+
     // Generar Modal
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
     modal.onclick = cerrarModal
 
+    // Botón cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar');
+    cerrarModalBtn.onclick = cerrarModal;
+
+    modal.appendChild(imagen);
+    modal.appendChild(cerrarModalBtn);
+    
+
     // Agregar al HTML
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
     body.appendChild(modal)
 }
 
-function cerrarModal(){
+function cerrarModal() {
     const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out');
 
-    modal?.remove()
+    setTimeout(() => {
+        modal?.remove()
+
+        //Remover el anti scroll
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 300);
 }
