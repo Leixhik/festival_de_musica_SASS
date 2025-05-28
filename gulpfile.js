1461179cff0f659d47457702a2defc7850bfa053
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-05-28 17:33:32 -0600

    Se agregó la función de recorte de imágenes utilizando sharp, se actualizaron las dependencias en package.json y package-lock.json, y se generaron miniaturas para la galería de imágenes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
+import path from 'path'
+import fs from 'fs'
 import {src, dest, watch, series} from 'gulp'; // src y dest son funciones de gulp que permiten definir tareas para procesar archivos. src se utiliza para especificar la ubicación de los archivos de origen, mientras que dest se utiliza para definir la ubicación de salida donde se guardarán los archivos procesados.
-
 import * as dartSass from 'sass'; // Dart Sass es una implementación de Sass escrita en Dart. Es la versión recomendada de Sass y es compatible con todas las características del lenguaje Sass. Dart Sass se utiliza para compilar archivos SCSS o Sass a CSS.
-
 import gulpSass from "gulp-sass";
 /* utiliza la sintaxis de módulos de JavaScript (ES Modules) para importar el paquete gulp-sass. Este paquete es un plugin para Gulp (un sistema de automatización de tareas) que permite compilar archivos SCSS o Sass a CSS. Sass es un preprocesador de CSS que añade características como variables, anidación y mixins, lo que facilita la escritura y mantenimiento de estilos. 
 */
@@ -9,6 +9,7 @@ import gulpSass from "gulp-sass";
 const sass = gulpSass(dartSass);
 
 import terser from 'gulp-terser'
+import sharp from 'sharp'
 
 export function js( done){
 
@@ -30,10 +31,38 @@ export function css(done){
     done();
 }
 
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export function dev(){
     watch('src/scss/**/*.scss', css);/* La línea observa cambios en el archivo app.scss y ejecuta la tarea css cuando detecta modificaciones */
     watch('src/js/**/*.js', js);
     
 }
 
-export default series( js, css, dev)
+export default series( crop, js, css, dev)
