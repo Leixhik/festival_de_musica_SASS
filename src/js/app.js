d28efe5be8b6a67529431a74c69565564e9dbdd4
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-05-29 15:04:00 -0600

    Refactor image handling in gallery and main section to use <picture> for AVIF and WebP support

--- src/js/app.js
+++ src/js/app.js
@@ -24,12 +24,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for (let i = 1; i <= cantidadImagenes; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy'
-        imagen.width = "300"
-        imagen.height = "200"
-        imagen.src = `src/img/gallery/thumb/${i}.jpg`
-        imagen.alt = 'Imagen Galería'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
 
         //Event Handler
         imagen.onclick = function () {
@@ -41,9 +41,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG');
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = 'Imagen ${id} en tamaño completo';
+    const imagen = document.createElement('PICTURE');
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
 
     // Generar Modal
     const modal = document.createElement('DIV')
@@ -89,14 +92,14 @@ function resaltarEnlace() {
             const sectionTop = section.offsetTop
             const sectionHeight = section.clientHeight
 
-            if(window.scrollY >= (sectionTop - sectionHeight / 3 )){
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
                 actual = section.id
             }
         })
 
         navLinks.forEach(link => {
             link.classList.remove('active')
-            if(link.getAttribute('href') === '#' + actual){
+            if (link.getAttribute('href') === '#' + actual) {
                 link.classList.add('active')
             }
         })
@@ -104,15 +107,15 @@ function resaltarEnlace() {
 }
 
 function scrollNav() {
-    const navLinks = document.querySelectorAll('.navegacion-principal a') 
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
 
-    navLinks.forEach( link => {
-        link.addEventListener('click', e =>{
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
             e.preventDefault() // para eliminar el comportamiento default
             const sectionScroll = e.target.getAttribute('href')
             const section = document.querySelector(sectionScroll)
-            
-            section.scrollIntoView({behavior: 'smooth'})
+
+            section.scrollIntoView({ behavior: 'smooth' })
         })
     })
 }
